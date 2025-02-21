b557810da3de9b167582153e2a31c598ad2137a7
v1ncer3 <andersonjrignacio@gmail.com>
2025-02-21 14:28:37 -0300

    feat(frontend): adicionar estrutura inicial do Vue com Vuetify

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-btn color="primary">Clique Aqui</v-btn>
+    </v-container>
+  </v-app>
+</template>
---
+++ src/frontend/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+
+const vuetify = createVuetify();
+
+createApp(App).use(vuetify).mount('#app');
